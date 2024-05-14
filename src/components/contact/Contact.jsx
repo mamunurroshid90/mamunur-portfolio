91ee74f66d2ad27740ac2bb0b11770c4f8b63364
Mamunur Roshid <69715351+mamunurroshid90@users.noreply.github.com>
2024-05-14 21:58:47 +0600

    added contact form right part

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,13 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Title from "../layouts/Title";
 import ContactLeft from "./ContactLeft";
 
 const Contact = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const emailValidation = () => {
+    return String(email)
+      .toLocaleLowerCase()
+      .match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g);
+  };
+
+  const handleSend = (e) => {
+    e.preventDefault();
+    if (username === "") {
+      setError("Username is required");
+    } else if (phoneNumber === "") {
+      setError("Phone number is required");
+    } else if (email === "") {
+      setError("Email is required");
+    } else if (!emailValidation(email)) {
+      setError("Give a valid Email");
+    } else if (subject === "") {
+      setError("Subject is required");
+    } else if (message === "") {
+      setError("Message is required");
+    } else {
+      setSuccess(
+        `Thank you dear ${username}, your message has been successfully`
+      );
+      setError("");
+      setUsername("");
+      setPhoneNumber("");
+      setEmail("");
+      setSubject("");
+      setMessage("");
+      //   console.log(username, phoneNumber, email, subject, message);
+    }
+  };
+
   return (
     <section
       id="contacts"
-      className=" w-full py-20 border-b-[1px] border-b-black"
+      className="w-full py-20 border-b-[1px] border-b-black"
     >
       <div className=" flex justify-center items-center text-center">
         <Title title="CONTACT" desc="Contact With Me" />
@@ -15,7 +57,109 @@ const Contact = () => {
       <div className=" w-full">
         <div className=" w-full h-auto flex justify-between">
           <ContactLeft />
-          <div className=" w-[60%] h-full flex flex-col justify-between bg-blue-300"></div>
+          <div className=" w-[60%] h-full py-10 flex flex-col justify-between">
+            <form className=" flex flex-col gap-6 py-5">
+              {error && (
+                <p className=" py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-orange-500 text-base tracking-wide animate-bounce">
+                  {error}
+                </p>
+              )}
+              {success && (
+                <p className=" py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-green-500 text-base tracking-wide animate-bounce">
+                  {success}
+                </p>
+              )}
+              <div className=" flex gap-10">
+                <div className=" w-1/2 flex flex-col gap-4">
+                  <p className=" text-sm text-gray-400 uppercase tracking-wide">
+                    your name
+                  </p>
+                  <input
+                    onChange={(e) => setUsername(e.target.value)}
+                    value={username}
+                    className={`${
+                      error === "Username is required" && "outline-designColor"
+                    } contactInput`}
+                    type="text"
+                  />
+                </div>
+                <div className=" w-1/2 flex flex-col gap-4">
+                  <p className=" text-sm text-gray-400 uppercase tracking-wide">
+                    phone number
+                  </p>
+                  <input
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                    value={phoneNumber}
+                    className={`${
+                      error === "Phone number is required" &&
+                      "outline-designColor"
+                    } contactInput`}
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className=" flex flex-col gap-4">
+                <p className=" text-sm text-gray-400 uppercase tracking-wide">
+                  email
+                </p>
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                  className={`${
+                    error === "Email is required" && "outline-designColor"
+                  } contactInput`}
+                  type="email"
+                />
+              </div>
+              <div className=" flex flex-col gap-4">
+                <p className=" text-sm text-gray-400 uppercase tracking-wide">
+                  subject
+                </p>
+                <input
+                  onChange={(e) => setSubject(e.target.value)}
+                  value={subject}
+                  className={`${
+                    error === "Subject is required" && "outline-designColor"
+                  } contactInput`}
+                  type="text"
+                />
+              </div>
+              <div className=" flex flex-col gap-4">
+                <p className=" text-sm text-gray-400 uppercase tracking-wide">
+                  message
+                </p>
+                <textarea
+                  onChange={(e) => setMessage(e.target.value)}
+                  value={message}
+                  className={`${
+                    error === "Message is required" && "outline-designColor"
+                  } contactTextarea`}
+                  id=""
+                  cols="30"
+                  rows="8"
+                ></textarea>
+              </div>
+              <div className="w-full">
+                <button
+                  type="submit"
+                  onClick={handleSend}
+                  className=" w-full h-12 bg-[#141518] rounded-lg text-base text-gray-400 tracking-wider uppercase hover:text-white duration-300 hover:border-[1px] hover:border-designColor border-transparent"
+                >
+                  send message
+                </button>
+              </div>
+              {error && (
+                <p className=" py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-orange-500 text-base tracking-wide animate-bounce">
+                  {error}
+                </p>
+              )}
+              {success && (
+                <p className=" py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-green-500 text-base tracking-wide animate-bounce">
+                  {success}
+                </p>
+              )}
+            </form>
+          </div>
         </div>
       </div>
     </section>
